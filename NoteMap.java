37fad5e149d72aabfc81478874889f16d0e20ecf
markyuh <markbay@colostate.edu>
2024-05-13 01:03:38 -0600

    Added NoteMap class & checks for correct note

---
+++ NoteMap.java
@@ -0,0 +1,26 @@
+package PianoGame;
+import java.util.HashMap;
+
+public class NoteMap {
+    public static HashMap<Integer, String> numberToNote = new HashMap<>();
+
+    static {
+        numberToNote.put(1, "A");
+        numberToNote.put(2, "B");
+        numberToNote.put(3, "C");
+        numberToNote.put(4, "D");
+        numberToNote.put(5, "E");
+        numberToNote.put(6, "F");
+        numberToNote.put(7, "G");
+        numberToNote.put(8, "A");
+        numberToNote.put(9, "B");
+        numberToNote.put(10, "C");
+        numberToNote.put(11, "D");
+        numberToNote.put(12, "E");
+        numberToNote.put(13, "F");
+        numberToNote.put(14, "G");
+        numberToNote.put(15, "A");
+        numberToNote.put(16, "B");
+        numberToNote.put(17, "C");
+    }
+}
