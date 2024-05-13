9edd9b8e9fcc77f90b43d6830e25255ee21d37fe
markyuh <markbay@colostate.edu>
2024-05-13 15:53:40 -0600

    Added formatting

--- GamePanel.java
+++ GamePanel.java
@@ -1,10 +1,12 @@
 package PianoGame;
-import java.awt.*;//this imports everything in java.awt. no need to import graphics
-import java.awt.event.*;//no need to import action event, action listener, key adapter, key event
-import javax.swing.*;//jpanel, jbutton
-import javax.swing.border.EmptyBorder;//padding for images
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;//might be able to remove
 import java.util.Random;
-public class GamePanel extends JPanel implements ActionListener{
+
+public class GamePanel extends JPanel implements ActionListener {
     static final int SCREEN_WIDTH = 1200;
     static final int SCREEN_HEIGHT = 700;
     private Image checkMarkImage;
@@ -19,26 +21,26 @@ public class GamePanel extends JPanel implements ActionListener{
     private static final int FEEDBACK_X = 800;
     private static final int FEEDBACK_Y = 100;
 
-    GamePanel(){
+    GamePanel() {
         this.setLayout(null);
         rand = new Random();
-        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));//panel size
-        this.setBackground(Color.black);//background color
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));// panel size
+        this.setBackground(Color.black);// background color
         this.setFocusable(true);
         this.addMouseListener(new MyMouseAdapter());
 
         CalcNote.init();
 
         JLabel keyboardLabel = makeKeyboard();
-        keyboardLabel.setBounds(175, 460, 850, 240); 
+        keyboardLabel.setBounds(175, 460, 850, 240);
         this.add(keyboardLabel);
 
         JButton rollButton = makeRoll();
-        rollButton.setBounds(550, 360, 117, 29); 
+        rollButton.setBounds(550, 360, 117, 29);
         this.add(rollButton);
 
         JLabel noteLabel = makeNote();
-        noteLabel.setBounds(450, 40, 300, 300); 
+        noteLabel.setBounds(450, 40, 300, 300);
         this.add(noteLabel);
 
         ImageIcon checkMarkIcon = new ImageIcon(getClass().getResource("graphics/CheckMark.png"));
@@ -48,10 +50,12 @@ public class GamePanel extends JPanel implements ActionListener{
 
         startGame();
     }
-    public void startGame(){
+
+    public void startGame() {
         running = true;
     }
-    public JLabel makeKeyboard(){
+
+    public JLabel makeKeyboard() {
         ImageIcon originalImage = new ImageIcon(getClass().getResource("graphics/Keyboard.png"));
         Image scalingImage = originalImage.getImage().getScaledInstance(850, 240, Image.SCALE_SMOOTH);
         ImageIcon image = new ImageIcon(scalingImage);
@@ -60,48 +64,54 @@ public class GamePanel extends JPanel implements ActionListener{
 
         return displayField;
     }
-    public JButton makeRoll(){
+
+    public JButton makeRoll() {
         JButton rollButton = new JButton("Click to roll");
-        rollButton.setBounds(550,360,117,29);
+        rollButton.setBounds(550, 360, 117, 29);
         rollButton.addActionListener(this);
         return rollButton;
     }
-    public JLabel makeNote(){
+
+    public JLabel makeNote() {
         noteLabel = new JLabel();
-        noteLabel.setBounds(100, 200, 300, 300); 
+        noteLabel.setBounds(100, 200, 300, 300);
         notePic = new ImageIcon(getClass().getResource("graphics/notes/0.png"));
         Image scalingImage = notePic.getImage().getScaledInstance(300, 300, Image.SCALE_SMOOTH);
         ImageIcon scaledIcon = new ImageIcon(scalingImage);
         noteLabel.setIcon(scaledIcon);
         return noteLabel;
     }
-    public void paintComponent(Graphics g){
+
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         draw(g);
         if (showFeedback && feedbackImage != null) {
             g.drawImage(feedbackImage, FEEDBACK_X, FEEDBACK_Y, this);
         }
     }
-    public void draw(Graphics g){
-        g.setFont(new Font("Ink Free",Font.BOLD,30) );
+
+    public void draw(Graphics g) {
+        g.setFont(new Font("Ink Free", Font.BOLD, 30));
         FontMetrics metrics = getFontMetrics(g.getFont());
-        g.drawString("Score: ", (SCREEN_WIDTH-metrics.stringWidth("Score :"))/2, g.getFont().getSize()); 
+        g.drawString("Score: ", (SCREEN_WIDTH - metrics.stringWidth("Score :")) / 2, g.getFont().getSize());
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         rand = new Random();
-        noteNum = rand.nextInt(17) + 1; 
+        noteNum = rand.nextInt(17) + 1;
         notePic = new ImageIcon(getClass().getResource("graphics/notes/" + noteNum + ".png"));
         Image scalingImage = notePic.getImage().getScaledInstance(300, 300, Image.SCALE_SMOOTH);
         ImageIcon scaledIcon = new ImageIcon(scalingImage);
-        noteLabel.setIcon(scaledIcon); 
-        noteLabel.revalidate(); 
+        noteLabel.setIcon(scaledIcon);
+        noteLabel.revalidate();
         repaint();
 
     }
-    public class MyMouseAdapter extends MouseAdapter{
+
+    public class MyMouseAdapter extends MouseAdapter {
         @Override
-        public void mousePressed(MouseEvent e){
+        public void mousePressed(MouseEvent e) {
             int xVal = e.getX();
             int yVal = e.getY();
 
@@ -114,35 +124,18 @@ public class GamePanel extends JPanel implements ActionListener{
                         break;
                     }
                     showFeedback = true;
-        
 
                     if (clickedNote.equals(expectedNote)) {
                         feedbackImage = checkMarkImage;
                         System.out.println("Correct!");
-                    }
-                    else {
+                    } else {
                         feedbackImage = xMarkImage;
                         System.out.println("Incorrect! Expected: " + expectedNote + ", but clicked: " + clickedNote);
                     }
                     repaint();
-                    break; 
+                    break;
                 }
             }
-
-            // e.getLocationOnScreen();
-            //     int xVal = e.getX();
-            //     int yVal = e.getY();
-            // System.out.println("X:" + xVal + ", Y:" + yVal);
-
-            
-            // int xVal = e.getX();
-            // int yVal = e.getY();
-            // for (Rectangle key : CalcNote.noteMap.keySet()) {
-            //     if (key.contains(xVal, yVal)) {
-            //         System.out.println("Note: " + CalcNote.noteMap.get(key)); // Print the note corresponding to the clicked area
-            //         break; // Stop checking after the first match
-            //     }
-            // }
         }
     }
 }
